a50a87d6beec017c4b6ca59d30ff6afa8c907182
triinth <triintahk@gmail.com>
2023-04-20 15:28:29 +0300

    Project setup

---
+++ App.xaml.cs
@@ -0,0 +1,39 @@
+﻿#if WINDOWS
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Windows.Graphics;
+#endif
+
+
+namespace matkateenused;
+
+// Maasik ToDo äpi akna suuruse seadistamine
+public partial class App : Application
+{
+
+    const int WindowWidth = 540;
+    const int WindowHeight = 1150;
+
+    public App()
+    {
+        InitializeComponent();
+
+#if WINDOWS
+
+        Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
+        {
+            var mauiWindow = handler.VirtualView;
+            var nativeWindow = handler.PlatformView;
+            nativeWindow.Activate();
+            IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+            WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandle);
+            AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+            appWindow.Resize(new SizeInt32(WindowWidth, WindowHeight));
+        });
+#endif
+
+
+
+        MainPage = new NavigationPage(new StartPage());
+    }
+}
---
+++ GlobalUsings.cs
@@ -0,0 +1,11 @@
+﻿global using matkateenused.Views;
+global using matkateenused.Models;
+global using matkateenused.Services;
+
+
+global using Microsoft.Maui;
+global using Microsoft.Maui.Controls;
+global using Microsoft.Maui.Controls.Hosting;
+global using Microsoft.Maui.Hosting;
+global using Microsoft.Maui.Graphics;
+global using Microsoft.Maui.LifecycleEvents;
