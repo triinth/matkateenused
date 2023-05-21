05ebffa4c1788d6bddfe91195fd2522fcd66394d
Mannuir <vasarik@gmail.com>
2023-05-21 13:13:40 +0300

    update HikeServices
    added content

--- matkateenused/Services/HikesServices.cs
+++ matkateenused/Services/HikesServices.cs
@@ -9,9 +9,9 @@ namespace matkateenused.Services
             new()
             {
                 Name = "Militaarmatkad",
-                HeroImage = "",
+                HeroImage = "Brita.jpg",
                 Description = "Tere tulemast militaarmatkale Paldiski linnas! Lähete sügavale metsa, kus instruktorid juhendavad teid läbi rasketes tingimustes, mis simuleerivad tõelisi sõjalisi stsenaariume. Matk hõlmab relvade kasutamist, sõidukite juhtimist ja taktikalisi otsuseid. Ületate takistusi, külastate mahajäetud sõjaväeobjekte ja lahendate meeskonnatöö väljakutseid. Saate kogemusi ja oskusi, mis annavad vajalikud teadmised ja enesekindluse rasketes olukordades hakkama saamiseks. See on tõeline adrenaliin ja seiklus!",
-                Guide = "Marta Vasarik",
+                Guide = "Brita Taff",
                 Price = 100,
                 AccentColorStart = Color.FromArgb("#353535"),
                 AccentColorEnd = Color.FromArgb("#8d9098"),
@@ -26,9 +26,9 @@ namespace matkateenused.Services
             new()
             {
                 Name = "Kanuumatkad",
-                HeroImage = "",
-                Description = "",
-                Guide = "",
+                HeroImage = "Renata.jpg",
+                Description = "Tere tulemast kanuumatkale Eestimaa jõgedel - seiklus, mida sa ei unusta kunagi! Meie matkad sobivad ideaalselt kogu perele, kuid oleme mõelnud ka nõudlikumatele adrenaliinihuvilistele. Meie kogenud instruktorid aitavad teil valmistuda ja õpetavad teile kanuumatka aluseid, et tunneksite end looduse ilu nautides mugavalt ning turvaliselt. Olenemata sellest, kas soovite nautida rahulikku sõitu mööda jõge või otsite seiklust ja adrenaliini, pakume teile unustamatuid kanuumatka kogemusi Eestimaa jõgedel. Tule ja ühine meiega seikluseks!\r\n",
+                Guide = "Renata Vool",
                 Price = 130,
                 AccentColorStart = Color.FromArgb("#a6393b"),
                 AccentColorEnd = Color.FromArgb("#d17f21"),
@@ -43,9 +43,9 @@ namespace matkateenused.Services
             new()
             {
                 Name = "Loodusmatkad",
-                HeroImage = "",
-                Description = "",
-                Guide = "",
+                HeroImage = "Uma.jpg",
+                Description = "Tere tulemast imelisse Eestimaa loodusesse, kus iga nurk on täis ilu ja võimalusi! Meie matkad viivad sind läbi metsade, rabade, järvede ja mägede, avastades meie looduse mitmekesisust ja võlu. Oled oodatud meiega koos kogema midagi, mis jääb sinuga kogu eluks. Matkad toimuvad nii jalgsi kui räätsadel, fotokaamera kaasas või ilma, ühe päeva jalutades või kuuvalgel telkides. Meie parimad loodusgiidid juhendavad ja aitavad sul tundma õppida Eestimaa looduse floorat ja faunat. Kas oled valmis avastama Eestimaa looduse võlumaailma koos meiega? Ootame sind!\r\nMatkajuht Uma!\r\nUma on särav ja sõbralik inimene, kes armastab loodust ja kõike sellega seonduvat. Ta  on suurepärane jutuvestja ning oskab rääkida kaasahaaravaid lugusid, samas oskuslikult tutvustades Eestimaa looduse hästi hoitud saladusi. \r\n",
+                Guide = "Uma Rada",
                 Price = 50,
                 AccentColorStart = Color.FromArgb("#0e3d68"),
                 AccentColorEnd = Color.FromArgb("#2e97c7"),
