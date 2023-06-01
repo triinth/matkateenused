126aeb90ded5ee0d2ff7f6a4d41a493f37172fd0
amaasik <75305698+amaasik@users.noreply.github.com>
2023-06-01 09:09:14 +0300

    picture fit

--- matkateenused/Services/HikesServices.cs
+++ matkateenused/Services/HikesServices.cs
@@ -66,7 +66,7 @@ namespace matkateenused.Services
                 Images = new List<string>
                 {
                     "telk.png",
-                    "jarv.png",
+                    "jarv.jpg",
                     "saar.png"
                 }
             }
